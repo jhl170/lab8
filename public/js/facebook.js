7cab5362c78c8d72226c232f8ac1ccfdee9d24f4
Jun Lee <jhl170@ucsd.edu>
2017-03-02 01:52:35 -0800

    thirdpartyAPI lab

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -21,5 +21,5 @@ function changeUser(response) {
   console.log(response);
   $('.facebookLogin').hide("p");
   $("#name").text(response.name);
-  $("#photo").attr("src", "https://scontent.xx.fbcdn.net/v/t1.0-1/p480x480/12140755_10153739321334697_3181428523284105369_n.jpg?oh=45db2b73230ab407c26a0b064bb23e5c&oe=59379FDA");
+  $("#photo").attr("src", "response.picture.data.url");
 }
